77828309fe8b32761a5b14e9b5b84fe3ba876c37
kieron <kieron>
2002-05-06 11:57:37 +0000

    killall

--- externgen2/externfunc.h
+++
@@ -1,8 +0,0 @@
-typedef unsigned int bool;
-#define TRUE 1
-#define FALSE 0
-
-extern char *output_lineending;
-
-extern bool analyse_functions(char *filename, FILE *outfile);
-extern bool analyse_variables(char *filename, FILE *outfile);
