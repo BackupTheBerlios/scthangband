14719b017fcedf2a31bc139a40c5952bdcea7e89
kieron <kieron>
2003-08-03 21:57:57 +0000

    Fix a silly bug which prevented bones files from being created (again).

--- cthangband/src/files.c
+++ cthangband/src/files.c
@@ -4787,7 +4787,7 @@ static void make_bones(void)
 	FILE                *fp;
 
 	/* Undead can't be raised as undead. */
-	if (player_is_undead) return;
+	if (player_is_undead()) return;
 
 	/* Ignore wizards and borgs */
 	if (!(noscore & 0x00FF))
